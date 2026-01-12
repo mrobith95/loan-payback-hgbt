5e192a9be5a8f086ee6d5f364a0a7cde629ad8c8
Muhammad Robith <nomadpanda0@gmail.com>
2026-01-12 22:04:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 import numpy as np
 import pandas as pd
 from joblib import load
+import skops.io as sio
 import gradio as gr
 import shap
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.ensemble import HistGradientBoostingClassifier
+from google import genai
+import os
 
 ## versions
-# numpy 1.26.4
-# pandas 2.2.3
-# joblib 1.5.2
-# gradio 5.38.1 
+# shap 0.49.1
+# scikit-learn 1.6.1
+# numpy 2.0.2
+# pandas 2.2.2
+# skops 0.13.0
+# joblib 1.5.3
+# gradio 5.38.1
 
 ## functions for shap
 def predict_log_proba(z):
@@ -19,13 +27,40 @@ def predict_proba(z):
     p = model.predict_proba(z)
     return p[:,1]
 
-model = load('best_model.joblib')
-enc = load('encoder.joblib')
+model_path = 'model.skops'
+enc_path   = 'encoder.skops'
+unknown_model = sio.get_untrusted_types(file=model_path)
+unknown_enc   = sio.get_untrusted_types(file=enc_path)
+model = sio.load(model_path, trusted=unknown_model)
+enc   = sio.load(  enc_path, trusted=unknown_enc)
+
 explainer = load('explainer.joblib')
-# ## additionals
-# categoricals = load('/kaggle/input/best-model-v2/scikitlearn/default/1/categoricals.joblib')
-# features = load('/kaggle/input/best-model-v2/scikitlearn/default/1/features.joblib')
-# monotonics = load('/kaggle/input/best-model-v2/scikitlearn/default/1/motonics.joblib')
+exist_shap = load('shap_values.joblib')
+
+## read pdf file
+pdf_path = 'SHAP_scatter_plot.pdf'
+
+# display(model)
+# display(enc)
+# display(explainer)
+# display(exist_shap)
+
+api_key = os.getenv("GEMINI_KEY")
+client = genai.Client(api_key=api_key) ## initailize client
+
+print("Uploading file...")
+uploaded_file = client.files.upload(file=pdf_path)
+
+while uploaded_file.state.name == "PROCESSING":
+    print(".", end="", flush=True)
+    time.sleep(2)
+    uploaded_file = client.files.get(name=uploaded_file.name)
+
+if uploaded_file.state.name == "FAILED":
+    raise Exception("File processing failed.")
+
+print("\nFile ready!")
+
 
 ## notes on input
 # user might not know about debt-to-income-ratio, credit-score, interest_rate, grade_subgrade
@@ -92,31 +127,40 @@ def do_this(ai, debt, lo, g, ms, el, es, lp):
 
     pred_pd = f"{100*float(pred_p[0]):.2f}%"
 
-    ## shap values
-    shap_values = explainer(masuk_pd)
+    if pred_text == 'Reject':
 
-    ## give rejection message
-    rej_mes = ""
-    if pred_p[0]<0.5: ## this meant that application rejected
-        ## debt-to-income ratio
-        if shap_values.values[0][1]<0 and shap_values.data[0][1]>=0.2:
-            rej_mes += "Your current debt is too high. Pay some of your debt first, or try to have more income.\n"
-        elif shap_values.values[0][1]<0 and shap_values.data[0][1]<0.2:
-            rej_mes += "Your current debt is too high for this application. Pay some of your debt first, or try to have more income.\n"
-            
-        ## credit-score
-        if shap_values.values[0][2]<0:
-            rej_mes += "Your credit score is too low for this application. Consider looking for more lenient lending services and pay your credit in time to improve your score.\n"
+        ## shap values
+        shap_values = explainer(masuk_pd)
+
+        print(shap_values)
+
+        prom = f'''
+        Explain why my loan application has low chance to be accepted.
+        Then, suggest me some approach to improve my chance of approval based on the result.
+        Make sure that your explanation is easy to understand and avoid technical terms whenever possible.
+        Here is the data given by company for you to analyze.
+        Debt-to-Income Ratio: {masuk['debt_to_income_ratio']:.3f}, SHAP {shap_values.values[:,0].item():.3f}
+        Credit Score: {masuk['credit_score']:.3f}, SHAP {shap_values.values[:,1].item():.3f}
+        Loan Amount: {masuk['loan_amount']:.3f}, SHAP {shap_values.values[:,2].item():.3f}
+        Marital Status: {masuk['marital_status']}, SHAP {shap_values.values[:,3].item():.3f}
+        Employment Status: {masuk['employment_status']}, SHAP {shap_values.values[:,4].item():.3f}
+        SHAP base value: {shap_values.base_values.item():.3f}
+        SHAP value vs. Feature plot on all features and Basic analysis given in the document.
+        Categorical features and their encoding can be seen in the document.
+        '''
         
-        ## loan-amount
-        if shap_values.data[0][3]>38000:
-            rej_mes += "Loan amount you propose is too high. Consider lowering your loan amount.\n"
+        response = client.models.generate_content(
+            model="gemini-2.5-flash-lite", 
+            contents=[uploaded_file, prom]
+        )
+    
+        # rej_mes = ""
+        rej_mes = response.text
 
-        ## deafult text
-        if len(rej_mes)==0:
-            rej_mes = "Sorry, we have no suggestion on how to improve your application."
     else:
-        rej_mes = "Congratulations! Your application has a good chance to be accepted!"
+        rej_mes = "**Congratulations!** Your application have a good chance to be accepted!"
+
+    rej_mes = "**Messages:**<br>"+rej_mes
     
     return pred_text, pred_pd, rej_mes
 
@@ -142,7 +186,8 @@ with gr.Blocks() as demo:
     outputs=[
         gr.Textbox(max_lines=1, label='Prediction', show_label=True),
         gr.Textbox(max_lines=1, label='Acceptance Probability', show_label=True),
-        gr.Textbox(max_lines=6, label='Message', show_label=True)
+        # gr.Textbox(max_lines=6, label='Rejection Message', show_label=True)
+        gr.Markdown(value="**Messages:**<br>", label='Rejection Message', show_label=True, container=True)        
         ]
     tombol.click(do_this, inputs, outputs)
     gr.Markdown(
